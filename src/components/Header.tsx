7e4c5264153fa1d2d791eba52e627e8f65badf5b
Amol <shelkeamol897@gmail.com>
2023-10-10 18:59:04 +0530

    [feat]: Navbar created and loading effect added

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header
+      className="bg-white shadow-lg h-[90px] 
+    sticky flex flex-row justify-between items-center px-32">
+      <div className="flex items-center">
+        <div className="h-[60px] w-[60px]">
+          <Image
+            src={"/images/logo.jpg"}
+            height={100}
+            width={100}
+            alt="logo"
+            className=" rounded-full"
+          />
+        </div>
+
+        <h1
+          className="text-black text-2xl mx-4 uppercase font-semibold 
+        tracking-wide">
+          Amol Shelke
+        </h1>
+      </div>
+
+      <div className=" w-1/4 mr-8 flex">
+        <ul className="flex justify-around w-full">
+          <Link
+            href={"#home"}
+            className="text-lg font-medium px-4 tracking-wide">
+            Home
+          </Link>
+          <Link
+            href={"#about"}
+            className="text-lg font-medium px-4 tracking-wide">
+            About
+          </Link>
+          <Link
+            href={"#projects"}
+            className="text-lg font-medium px-4 tracking-wide">
+            Projects
+          </Link>
+          <Link
+            href={"#blogs"}
+            className="text-lg font-medium px-4 tracking-wide">
+            Blogs
+          </Link>
+          <Link
+            href={"#contact"}
+            className="text-xl font-medium px-4 tracking-wide">
+            Contact
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
+import { Header } from "@/components/Header";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  return isLoading ? (
+    <div className="flex justify-center items-center h-screen">
+      <Image src={"/images/pre.svg"} height={150} width={150} alt="loading" />
+    </div>
+  ) : (
+    <div className="h-full w-full bg-white">
+      <Header />
+    </div>
+  );
 }
