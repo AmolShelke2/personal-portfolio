846d6db342a54953398a211a980ed4f102eadda4
Amol <shelkeamol897@gmail.com>
2023-10-12 21:56:55 +0530

    [feat]: responsiveness added for navbar, and home section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export const Header = () => {
+  const [isNavActive, setIsNavActive] = useState(false);
+
   return (
     <header
-      className="bg-white shadow-lg h-[90px] 
-    fixed top-0 left-0 w-full flex flex-row justify-between items-center px-32">
+      className="bg-white shadow-lg h-[80px] lg:h-[100px] fixed top-0 left-0 w-full flex flex-col
+     lg:flex-row lg:justify-between lg:items-center gap-8 lg:gap-0 lg:px-32">
       <div className="flex items-center">
-        <div className="h-[60px] w-[60px]">
+        <div className="h-[60px] w-[60px] mt-2 lg:mt-0">
           <Image
             src={"/images/logo.jpg"}
             height={100}
@@ -19,40 +22,49 @@ export const Header = () => {
         </div>
 
         <h1
-          className="text-black text-2xl mx-4 uppercase font-semibold 
+          className="text-black text-xl lg:text-2xl mx-4 uppercase font-semibold 
         tracking-wide">
           Amol Shelke
         </h1>
       </div>
 
-      <div className=" w-1/4 mr-8 flex">
-        <ul className="flex justify-around w-full">
+      <div className="w-full lg:w-1/4 mr-8 flex">
+        <ul
+          className={`${
+            isNavActive ? "flex  transition-all duration-150" : "hidden"
+          } lg:flex flex-col lg:flex-row justify-around w-full bg-slate-100 lg:bg-white`}>
           <Link
             href={"#home"}
-            className="text-lg font-medium px-4 tracking-wide">
+            className="text-lg font-medium px-4 py-4 lg:py-0 tracking-wide border-b-2 lg:border-none">
             Home
           </Link>
           <Link
             href={"#about"}
-            className="text-lg font-medium px-4 tracking-wide">
+            className="text-lg font-medium px-4 py-4 lg:py-0 tracking-wide border-b-2 lg:border-none">
             About
           </Link>
           <Link
             href={"#projects"}
-            className="text-lg font-medium px-4 tracking-wide">
+            className="text-lg font-medium px-4 py-4 lg:py-0 tracking-wide border-b-2 lg:border-none">
             Projects
           </Link>
           <Link
             href={"#blogs"}
-            className="text-lg font-medium px-4 tracking-wide">
+            className="text-lg font-medium px-4 py-4 lg:py-0 border-b-2 lg:border-none tracking-wide">
             Blogs
           </Link>
           <Link
             href={"#contact"}
-            className="text-xl font-medium px-4 tracking-wide">
+            className="text-xl font-medium px-4 py-4 lg:py-0 border-b-2 lg:border-none tracking-wide">
             Contact
           </Link>
         </ul>
+      </div>
+
+      <div
+        className="block lg:hidden absolute right-4 top-5 cursor-pointer"
+        onClick={() => setIsNavActive(!isNavActive)}>
+        <AiOutlineMenu size={25} />
       </div>
     </header>
   );
--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -4,11 +4,11 @@ import React from "react";
 export const HomeSection = () => {
   return (
     <div className="h-screen mb-8 bg-home">
-      <div className="max-w-[1440px] h-full flex flex-col justify-center items-center mx-auto">
-        <h2 className="text-black text-5xl p-8 font-extrabold uppercase tracking-wide mb-5 w-3/4 text-center">
+      <div className="max-w-[1440px] h-full flex flex-col justify-center items-center mx-auto px-4 lg:px-0">
+        <h2 className="text-black text-2xl p-2 lg:text-5xl lg:p-8 font-extrabold uppercase tracking-wide mb-5 lg:w-3/4 text-center">
           Hey there, I&apos;m Amol Shelke
         </h2>
-        <p className="w-2/4 text-xl leading-9 text-center mb-10">
+        <p className="lg:w-2/4 text-lg w-full sm:w-[70%] lg:text-xl lg:leading-9 text-center mb-10">
           A Frontend developer building user interfaces with Nextjs, Reactjs and
           Tailwind CSS. Which leads the success of overall product.
         </p>
