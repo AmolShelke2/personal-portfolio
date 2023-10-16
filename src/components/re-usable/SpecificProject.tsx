25dd9525aef269d5c08a936ed47d12d0b6a143a5
Amol <shelkeamol897@gmail.com>
2023-10-17 00:47:01 +0530

    [feat]: animations added

--- src/components/re-usable/SpecificProject.tsx
+++ src/components/re-usable/SpecificProject.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 
 interface specificProject {
   id: number;
@@ -22,13 +23,32 @@ export const SpecificProject: React.FC<specificProject> = ({
   projectMobilePreview,
   isReverse,
 }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+
   return (
     <div>
       <div
         className={`h-full lg:h-[650px] bg-[#f8fafc] flex flex-col-reverse ${
           isReverse ? "lg:flex-row-reverse" : "lg:flex-row"
         } justify-between gap-4 items-center py-4 my-4 shadow-md px-5`}>
-        <div className="w-full lg:w-1/2">
+        <motion.div
+          ref={ref}
+          variants={{
+            hidden: { opacity: 0, x: 76 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 1.5, delay: 0.25 }}
+          className="w-full lg:w-1/2">
           <h3 className="text-[32px] lg:text-[45px] font-bold my-2">
             {projectTitle}
           </h3>
@@ -48,9 +68,18 @@ export const SpecificProject: React.FC<specificProject> = ({
               </button>
             </Link>
           </div>
-        </div>
+        </motion.div>
 
-        <div className="w-full lg:w-1/2 py-4 my-4 relative z-10">
+        <motion.div
+          className="w-full lg:w-1/2 py-4 my-4 relative z-10"
+          ref={ref}
+          variants={{
+            hidden: { opacity: 0, y: 76 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 1, delay: 0.5 }}>
           <Image
             src={projectDesktopPreview}
             height={600}
@@ -66,7 +95,7 @@ export const SpecificProject: React.FC<specificProject> = ({
             alt="project-1"
             className="absolute bottom-0"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
