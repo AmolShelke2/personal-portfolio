f84cf7f852bc873d1c36e82052c692b095d969f3
Amol <shelkeamol897@gmail.com>
2023-10-16 19:59:46 +0530

    minor changes

--- src/components/re-usable/SpecificProject.tsx
+++ src/components/re-usable/SpecificProject.tsx
@@ -23,46 +23,48 @@ export const SpecificProject: React.FC<specificProject> = ({
   flexFlow,
 }) => {
   return (
-    <div
-      className={`h-full lg:h-[650px] bg-[#f8fafc] flex flex-col-reverse ${flexFlow} justify-between gap-4 items-center py-4 my-4 shadow-md px-5`}>
-      <div className="w-full lg:w-1/2">
-        <h3 className="text-[32px] lg:text-[45px] font-bold my-2">
-          {projectTitle}
-        </h3>
-        <p className="text-gray-600 text-lg lg:text-xl py-4 my-4 w-[90%]">
-          {projectDescription}
-        </p>
+    <div>
+      <div
+        className={`h-full lg:h-[650px] bg-[#f8fafc] flex flex-col-reverse ${flexFlow} justify-between gap-4 items-center py-4 my-4 shadow-md px-5`}>
+        <div className="w-full lg:w-1/2">
+          <h3 className="text-[32px] lg:text-[45px] font-bold my-2">
+            {projectTitle}
+          </h3>
+          <p className="text-gray-600 text-lg lg:text-xl py-4 my-4 w-[90%]">
+            {projectDescription}
+          </p>
 
-        <div className="flex items-center gap-4 py-4">
-          <Link href={projectDemo} target="_blank">
-            <button className="bg-[#e13019] text-white w-[180px] py-4 rounded-lg text-lg shadow-md">
-              Demo
-            </button>
-          </Link>
-          <Link href={projectSource} target="_blank">
-            <button className="bg-[#e13019] text-white w-[180px] py-4 rounded-lg text-lg shadow-md">
-              Source
-            </button>
-          </Link>
+          <div className="flex items-center gap-4 py-4">
+            <Link href={projectDemo} target="_blank">
+              <button className="bg-[#e13019] text-white w-[180px] py-4 rounded-lg text-lg shadow-md">
+                Demo
+              </button>
+            </Link>
+            <Link href={projectSource} target="_blank">
+              <button className="bg-[#e13019] text-white w-[180px] py-4 rounded-lg text-lg shadow-md">
+                Source
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
 
-      <div className="w-full lg:w-1/2 py-4 my-4 relative z-10">
-        <Image
-          src={projectDesktopPreview}
-          height={600}
-          width={600}
-          alt="project-1"
-          className="w-full h-full"
-        />
+        <div className="w-full lg:w-1/2 py-4 my-4 relative z-10">
+          <Image
+            src={projectDesktopPreview}
+            height={600}
+            width={600}
+            alt="project-1"
+            className="w-full h-full"
+          />
 
-        <Image
-          src={projectMobilePreview}
-          height={200}
-          width={100}
-          alt="project-1"
-          className="absolute bottom-0"
-        />
+          <Image
+            src={projectMobilePreview}
+            height={200}
+            width={100}
+            alt="project-1"
+            className="absolute bottom-0"
+          />
+        </div>
       </div>
     </div>
   );
