7a23fa84b12d169706f0cf155d0a444e9cd31037
Amol <shelkeamol897@gmail.com>
2023-10-17 23:26:46 +0530

    [changes]: minor changes

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
 
 export const HomeSection: React.FC = () => {
   return (
@@ -8,10 +9,13 @@ export const HomeSection: React.FC = () => {
         className="max-w-[1440px] h-full flex flex-col-reverse gap-8 lg:gap-6 sm:flex-row justify-center sm:justify-between 
       items-center mx-auto px-4 lg:px-0">
         <div className="w-full sm:w-1/2">
-          <h1 className="text-black p-2 text-3xl lg:text-[56px] lg:p-8 font-semibold uppercase mb-5 text-start lg:leading-[70px]">
+          <h1 className="text-black p-2 text-3xl lg:text-[56px] lg:p-8 font-semibold uppercase mb-5 text-center sm:text-start lg:leading-[70px]">
             Hey there, <br /> I&apos;m
             <span className=" text-blue-500 font-bold"> Amol Shelke</span>
           </h1>
+          <p className="text-center text-xl font-medium text-gray-600 capitalize tracking-wider sm:hidden">
+            Software engineer based in India
+          </p>
         </div>
 
         <div className="w-full sm:w-1/2">
