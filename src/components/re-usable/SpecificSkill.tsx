18f2b47b0ee9e8e4df36347ac36e2214d7782cf8
AmolShelke2 <shelkeamol897@gmail.com>
2023-11-02 12:28:00 +0530

    fixed: background colors of SpecificSkill component

--- src/components/re-usable/SpecificSkill.tsx
+++ src/components/re-usable/SpecificSkill.tsx
@@ -5,8 +5,8 @@ interface skillProp {
 export const SpecificSkill: React.FC<skillProp> = ({ skill }) => {
   return (
     <p
-      className="bg-slate-100 text-gray-500 text-center py-4 px-2 rounded-lg 
-        cursor-pointer hover:bg-slate-200 font-medium text-md shadow-md">
+      className="bg-slate-200 text-gray-500 text-center py-4 px-2 rounded-lg 
+        cursor-pointer hover:bg-slate-250 font-medium text-md shadow-md">
       {skill}
     </p>
   );
